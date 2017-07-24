a8daa24f2cf2396870610d00237489aed737c04e
chanyou0311 <yuukun0311@gmail.com>
2017-07-24 14:39:17 +0900

    first commit

---
+++ init.el
@@ -0,0 +1,25 @@
+(keyboard-translate ?\C-h ?\C-?)
+
+(when (eq system-type 'darwin)
+  (setq ns-command-modifier (quote meta)))
+
+(load-theme 'tango-dark' t)
+
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(add-to-list 'package-archives  '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+(package-initialize)
+
+(autoload 'python-mode "python-mode" "Python editing mode." t)
+(custom-set-variables
+  '(py-indent-offset 4)
+)
+(add-hook 'python-mode-hook
+  '(lambda()
+    (setq tab-width 4) 
+    (setq indent-tabs-mode nil)
+  )
+)
+
