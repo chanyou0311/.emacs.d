65f4a90de2228c8c3ed2087264a537b8ac200f27
chanyou0311 <yuukun0311@gmail.com>
2017-08-11 13:56:27 +0900

    Show nuber of lines

--- init.el
+++ init.el
@@ -12,6 +12,10 @@
     (setq ns-command-modifier (quote meta)))
 (load-theme 'tango-dark' t)
 
+;; 行番号の表示
+(global-linum-mode t)
+(setq linum-format "%4d:")
+
 ;; (require 'package)
 ;; (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 ;; (add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
