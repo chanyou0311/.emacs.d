38ab8b817c8f7cb77ad452f3615e29758e8e3780
chanyou0311 <yuukun0311@gmail.com>
2017-08-27 21:55:21 +0900

    modify web-mode

--- init.el
+++ init.el
@@ -73,7 +73,7 @@
 ;; virtualenvwrapper
 (require 'virtualenvwrapper)
 (require 'auto-virtualenvwrapper)
-(add-hook 'python-mode-hook #'auto-virtualenvwrapper-activate)
+(add-hook 'python-mode-hook 'auto-virtualenvwrapper-activate)
 
 
 ;; web-mode
@@ -94,6 +94,63 @@
   (setq web-mode-php-offset    2)
   (setq web-mode-java-offset   2)
   (setq web-mode-asp-offset    2))
+(add-hook 'web-mode-hook 'web-mode-hook)
+
+(defun web-mode-hook ()
+  "Hooks for Web mode."
+  ;; web-modeの設定
+  (setq web-mode-markup-indent-offset 2) ;; html indent
+  (setq web-mode-css-indent-offset 2) ;; css indent
+  (setq web-mode-code-indent-offset 2) ;; script indent(js,php,etc..)
+  ;; htmlの内容をインデント
+  ;; TEXTAREA等の中身をインデントすると副作用が起こったりするので
+  ;; デフォルトではインデントしない
+  ;;(setq web-mode-indent-style 2)
+  ;; コメントのスタイル
+  ;; 1:htmlのコメントスタイル(default)
+  ;; 2:テンプレートエンジンのコメントスタイル
+  ;; (Ex. {# django comment #},{* smarty comment *},{{-- blade comment --}})
+  (setq web-mode-comment-style 2)
+  ;; 終了タグの自動補完をしない
+  ;;(setq web-mode-disable-auto-pairing t)
+  ;; color:#ff0000;等とした場合に指定した色をbgに表示しない
+  ;;(setq web-mode-disable-css-colorization t)
+  ;;css,js,php,etc..の範囲をbg色で表示
+  ;; (setq web-mode-enable-block-faces t)
+  ;; (custom-set-faces
+  ;; '(web-mode-server-face
+  ;; ((t (:background "grey")))) ; template Blockの背景色
+  ;; '(web-mode-css-face
+  ;; ((t (:background "grey18")))) ; CSS Blockの背景色
+  ;; '(web-mode-javascript-face
+  ;; ((t (:background "grey36")))) ; javascript Blockの背景色
+  ;; )
+  ;;(setq web-mode-enable-heredoc-fontification t)
+  ;; auto tag closing
+  ;; 0=no auto-closing
+  ;; 1=auto-close with </
+  ;; 2=auto-close with > and </
+  (setq web-mode-auto-close-style 2)
+  (setq web-mode-tag-auto-close-style 2)
+  )
+
+
+
+;; 色の設定
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(web-mode-comment-face ((t (:foreground "#D30628")))) ;コメント
+ '(web-mode-css-at-rule-face ((t (:foreground "#FF7F00")))) ;CSSタグ
+ '(web-mode-css-pseudo-class-face ((t (:foreground "#FF7F00")))) ;CSS擬似クラス
+ '(web-mode-css-rule-face ((t (:foreground "#A0D8EF")))) ;CSSタグ
+ '(web-mode-doctype-face ((t (:foreground "#82AE46")))) ;docutype
+ '(web-mode-html-attr-name-face ((t (:foreground "#C97586")))) ;属性名など
+ '(web-mode-html-attr-value-face ((t (:foreground "#82AE46")))) ;属性値
+ '(web-mode-html-tag-face ((t (:foreground "##4682ae" :weight bold)))) ;要素名
+ '(web-mode-server-comment-face ((t (:foreground "#D9333F"))))) ;コメント
 (add-hook 'web-mode-hook 'web-mode-hook)
 
 (custom-set-faces
