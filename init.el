4dba3ffa774cb327b054e8e50a166f368d60f5f5
chanyou0311 <yuukun0311@gmail.com>
2017-10-06 11:09:39 +0900

    add pallet

--- init.el
+++ init.el
@@ -170,3 +170,15 @@
 ;; pallet
 (require 'pallet)
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (yasnippet web-mode-edit-element use-package smex smartparens python-mode projectile prodigy popwin pallet package-utils nyan-mode neotree multiple-cursors magit jedi-direx idle-highlight-mode htmlize flycheck-cask expand-region exec-path-from-shell emmet-mode drag-stuff auto-virtualenvwrapper))))
+
+;; magit
+(global-set-key (kbd "C-x g") 'magit-status)
+
