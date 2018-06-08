301b85b7580898baef38df0fc69fd62f33e0099a
chanyou0311 <yuukun0311@gmail.com>
2018-06-08 14:14:00 +0900

    [add] cssのインデントを2文字に変更

--- init.el
+++ init.el
@@ -144,6 +144,7 @@
 (add-to-list 'auto-mode-alist '("\\.as[cp]x$"   . web-mode))
 (add-to-list 'auto-mode-alist '("\\.erb$"       . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html?$"     . web-mode))
+(add-to-list 'auto-mode-alist '("\\.css?$"      . web-mode))
 ;;; インデント数
 (defun web-mode-hook ()
   "Hooks for Web mode."
@@ -152,7 +153,8 @@
   (setq web-mode-script-offset 2)
   (setq web-mode-php-offset    2)
   (setq web-mode-java-offset   2)
-  (setq web-mode-asp-offset    2))
+  (setq web-mode-asp-offset    2)
+)
 (add-hook 'web-mode-hook 'web-mode-hook)
 
 (defun web-mode-hook ()
